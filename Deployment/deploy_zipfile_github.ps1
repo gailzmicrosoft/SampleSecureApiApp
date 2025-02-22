151ce12963f54d2b84e1e0daffa17aa1ae819c47
Gaiye Zhou <gazho@microsoft.com>
2025-02-22 00:38:04 -0500

    First working version

--- Deployment/deploy_zipfile_github.ps1
+++ Deployment/deploy_zipfile_github.ps1
@@ -2,36 +2,34 @@
 # cd Deployment 
 # .\deploy_zipfile_github.ps1
 
-# This file is being tested right now. Not working yet
+# The PowerShell script downloads a zip file from a given URL and deploys it to an Azure App Service using the Azure CLI.
+# The script first logs in to Azure using the az login command. It then prompts the user for the resource group name and app service name.
+# After that, it downloads the zip file from the specified URL using the Invoke-WebRequest cmdlet and saves it to a temporary file.
+# Finally, it deploys the zip file to the specified Azure App Service using the Publish-AzWebApp cmdlet and cleans up the temporary file.
+#
+
+
+# Use GitHub Zip file 
+$zipFileUrl = 'https://raw.githubusercontent.com/gailzmicrosoft/SampleSecureApiApp/main/Deployment/sample_app.zip'
+
+# Set the path to the temporary file
+$zipFilePathTmp = [System.IO.Path]::GetTempFileName()
+Write-Host "Temporary file path: $zipFilePathTmp"
+$zipFilePathTmp = [System.IO.Path]::ChangeExtension($zipFilePathTmp, '.zip')
+Write-Host "Temporary file path with .zip: $zipFilePathTmp"
 
 az login
-
 # get user input
 $resourceGroup = Read-Host -Prompt "Enter your resource group name"
 $appServiceName = Read-Host -Prompt "Enter your app service name"
 
-#az webapp deploy --resource-group $resourceGroup --name $appServiceName --src-path $zipFilePath --type zip
 
-# Use GitHub Zip file 
-$zipFilePath = 'https://raw.githubusercontent.com/gailzmicrosoft/SampleSecureApiApp/main/Deployment/sample_app.zip'
+# Download the zip file
+Invoke-WebRequest -Uri $zipFileUrl -OutFile $zipFilePathTmp
 
-# Publish-AzWebApp -ResourceGroupName Default-Web-WestUS -Name MyApp -ArchivePath <zip-package-path>
+# Deploy the zip file
+Publish-AzWebApp -ResourceGroupName $resourceGroup -Name $appServiceName -ArchivePath $zipFilePathTmp
 
+# Clean up the downloaded zip file
+Remove-Item $zipFilePathTmp
 
-Publish-AzWebApp -ResourceGroupName $resourceGroup -Name $appServiceName -ArchivePath $zipFilePath
-
-# Error from above command:
-#The filename, directory name, or volume label syntax is incorrect. :
-#| 'C:\Repos\SampleSecureApiApp\Deployment\https:\raw.githubusercontent.com\gailzmicrosoft\SampleSecureApiApp\main\Deployment\sample_app.zip'
-
-
-# Not working
-#az webapp deploy --clean true --target-path "home/site/wwwroot/" --type zip -g $resourceGroup -n $appServiceName --src-path $zipFilePath
-# Error from above command:
-#Deploying from local path: https://raw.githubusercontent.com/gailzmicrosoft/SampleSecureApiApp/main/Deployment/sample_app.zip
-#Either 'https://raw.githubusercontent.com/gailzmicrosoft/SampleSecureApiApp/main/Deployment/sample_app.zip' is not a valid local file path or you do not have permissions to access it
-
-# Not working 
-# az webapp deploy --resource-group $resourceGroup --name $appServiceName --src-path $zipFilePath
-# Error from above command:
-# Either 'https://raw.githubusercontent.com/gailzmicrosoft/SampleSecureApiApp/main/Deployment/sample_app.zip' is not a valid local file path or you do not have permissions to access it
