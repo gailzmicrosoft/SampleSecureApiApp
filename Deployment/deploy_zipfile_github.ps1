87eb15f4195db7a4c99396847bda8d6f22c3cfee
Gaiye Zhou <gazho@microsoft.com>
2025-02-22 00:08:38 -0500

    Debugging

--- Deployment/deploy_zipfile_github.ps1
+++ Deployment/deploy_zipfile_github.ps1
@@ -2,7 +2,7 @@
 # cd Deployment 
 # .\deploy_zipfile_github.ps1
 
-# This file is being tested right now.
+# This file is being tested right now. Not working yet
 
 az login
 
@@ -13,6 +13,25 @@ $appServiceName = Read-Host -Prompt "Enter your app service name"
 #az webapp deploy --resource-group $resourceGroup --name $appServiceName --src-path $zipFilePath --type zip
 
 # Use GitHub Zip file 
-$gitHubZipFilePath = 'https://raw.githubusercontent.com/gailzmicrosoft/SampleApp/main/Deployment/sample_app.zip'
+$zipFilePath = 'https://raw.githubusercontent.com/gailzmicrosoft/SampleSecureApiApp/main/Deployment/sample_app.zip'
 
-az webapp deployment source config --resource-group $resourceGroup --name $appServiceName --src-path $gitHubZipFilePath --type zip
+# Publish-AzWebApp -ResourceGroupName Default-Web-WestUS -Name MyApp -ArchivePath <zip-package-path>
+
+
+Publish-AzWebApp -ResourceGroupName $resourceGroup -Name $appServiceName -ArchivePath $zipFilePath
+
+# Error from above command:
+#The filename, directory name, or volume label syntax is incorrect. :
+#| 'C:\Repos\SampleSecureApiApp\Deployment\https:\raw.githubusercontent.com\gailzmicrosoft\SampleSecureApiApp\main\Deployment\sample_app.zip'
+
+
+# Not working
+#az webapp deploy --clean true --target-path "home/site/wwwroot/" --type zip -g $resourceGroup -n $appServiceName --src-path $zipFilePath
+# Error from above command:
+#Deploying from local path: https://raw.githubusercontent.com/gailzmicrosoft/SampleSecureApiApp/main/Deployment/sample_app.zip
+#Either 'https://raw.githubusercontent.com/gailzmicrosoft/SampleSecureApiApp/main/Deployment/sample_app.zip' is not a valid local file path or you do not have permissions to access it
+
+# Not working 
+# az webapp deploy --resource-group $resourceGroup --name $appServiceName --src-path $zipFilePath
+# Error from above command:
+# Either 'https://raw.githubusercontent.com/gailzmicrosoft/SampleSecureApiApp/main/Deployment/sample_app.zip' is not a valid local file path or you do not have permissions to access it
--- README.md
+++ README.md
@@ -22,14 +22,14 @@ Secure Rest APIs hosted in Azure App Service. This is a sample architecture that
 
 In your local solution directory, change directory to **Deployment**. Run PowerShell script `build_zipfile.ps1` to rebuild the solution. Run `deploy_zipfile_local.ps1`  to deploy the new code as .zip file to your app service instance. 
 
-**Deploy Code from GitHub**
-
-In your local solution directory, change directory to **Deployment**. Run PowerShell script `deploy_zipfile_github.ps1`. This step is being automated by the 'Deploy to Azure' (under construction and test). In future this step will not be needed. The code kept as debugging script. 
-
 #### **Test the App**
 
 Find the URL of your APP Service in Azure. The API Key is stored in your App Configurator as the value of '`x-api-key`'. When the API client invokes the API, the x-api-key value pair must be constructed in the header. You can use swagger interface to test the APIs on a browner with the address of the App Service [Sample App Services UI](https://your-app-service-instance-name.azurewebsites.net/index.html).
 
 #### **Trouble Shooting**
 
-You may need to check your app services instance environment variables to make sure `AppConfig__ConnectionString` has the  correct value from your app configurator connection string. 
+If you downloaded or cloned the code locally and want to test the code locally first. You need to set up key vault policy to allow you to read the key and secret in the key vault. 
+
+**Deploy Code from GitHub - Under construction**
+
+In your local solution directory, change directory to **Deployment**. Run PowerShell script `deploy_zipfile_github.ps1`. This step is being automated by the 'Deploy to Azure' (under construction and test). In future this step will not be needed. The code kept as debugging script. 
